44435a48347808d6793df709de92a2d6594adec1
Charles <cukoh@byupathway.edu>
2026-02-06 19:10:08 +0100

    Remove config.js and .env from tracking - sensitive keys should not be in version control

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// Actual API keys
-
-
-export const API_KEYS = {
-    TICKETMASTER_KEY: 'qlTGz6aOmVZYTvwO7q041adJ1Ku7OuRO',
-    GOOGLE_MAPS_KEY: 'AIzaSyBGm4pv2fdu4X2z7c2ta1ToLv_SRbBpRdc'
-};
-
-export const BASE_URL = window.location.origin;
